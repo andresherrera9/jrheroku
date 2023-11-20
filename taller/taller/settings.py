d6924935d4873f21a3c237401d1ee6db6af90bb0
andres <andres.herrera92@hotmail.com>
2023-11-19 21:20:32 -0500

    check

--- taller/taller/settings.py
+++ taller/taller/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p)(me&4974-y%d%6(5-yrqer#vii_2qm8ys^svm7$m9vbk%9tw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['juegoderoles-baefdfaaed48.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
