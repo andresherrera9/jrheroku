22cfd859f7552e7156ac2c1d61c84bdc6ba35813
andres <andres.herrera92@hotmail.com>
2023-11-19 20:37:23 -0500

    check

--- taller/polls/views.py
+++ taller/polls/views.py
@@ -35,22 +35,23 @@ def index(request,polls_id,page_id):
                 if data_[x] < 0:
                     data_[x] = 0
            
-        path = Path('static/media/ROS_'+polls_id+'.csv')
-        
+        #path = Path('static/media/ROS_'+polls_id+'.csv')
+        path = Path(settings.MEDIA_ROOT) / f'ROS_{polls_id}.csv'
         if path.is_file():
-            df = pd.read_csv('static/media/ROS_'+polls_id+'.csv')
+            #df = pd.read_csv('static/media/ROS_'+polls_id+'.csv')
+            df = pd.read_csv(path)
             df2 = pd.DataFrame(data_)
             df2["fecha"] = current_time
             for k,v in data_.items():
                 df2[str(k)] = v         
                 df2[str(k)] = df2[str(k)].replace({'True':1,'False':0})
                 df_f = pd.concat([df,df2],ignore_index=True)
-                df_f.to_csv('static/media/ROS_'+polls_id+'.csv', index=False)
+                df_f.to_csv(path, index=False)
         else:
             
             df = pd.DataFrame.from_dict(data_)
             df["fecha"] = current_time
-            df.to_csv('static/media/ROS_'+polls_id+'.csv',index=False)
+            df.to_csv(path,index=False)
 
         messages.success(request,messages.INFO, 'Hola mundo')
         return redirect(request.path)
