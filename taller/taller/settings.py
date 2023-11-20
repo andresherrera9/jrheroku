6c7abdb2364ddaa0812ec256a260f8581fd38502
andres <andres.herrera92@hotmail.com>
2023-11-19 20:47:47 -0500

    check

--- taller/taller/settings.py
+++ taller/taller/settings.py
@@ -136,18 +136,11 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-# Media files (user uploads)
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 django_heroku.settings(locals())
 
 
 
-#STATIC_URL = "/static/"
-#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR,'static'),]
 
